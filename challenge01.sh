079b89c46356be6bc07b551f0a721ba0705b5084
Hector Cordova <hectorcordova2024@gmail.com>
2023-11-28 17:19:48 -0500

    finished challenge

--- challenge01.sh
+++ challenge01.sh
@@ -1,6 +1,25 @@
-# Copies /var/log/syslog to the current working directory 
-# and Appends the current date and time to the filename
-
 #!/bin/bash
 
->>syslog.txt
+# Script Name:                  challenge01
+# Author:                       Hector Cordova
+# Date of latest revision:      11/28/2023
+# Purpose:                      cp /var/log/syslog to pwd and Appends the 
+#                               current date and time to the file name 'syslog.txt'
+
+# Make the script itself executable using sudo
+sudo chmod +x "$0"
+
+# Declaration of variables
+
+# Declaration of functions
+
+# Main
+
+cat /var/log/syslog >> syslog.txt
+mv syslog.txt "$(date +'%Y-%m-%d')_syslog.txt"
+
+
+
+
+
+# End
