277ce18efe93bedc314770d136b5bc02966f178e
Hector Cordova <hectorcordova2024@gmail.com>
2023-12-01 20:51:27 -0500

    challenge05

---
+++ challenge05.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Script Name:                  challenge05
+# Author:                       Hector Cordova
+# Date of latest revision:      12/01/2023
+# Purpose:                     
+
+# Declaration of variables
+
+# Declaration of functions
+
+# Main
+
+#!/bin/bash
+
+backup_dir="/var/log/backups"
+timestamp=$(date +"%Y%m%d%H%M%S")
+
+echo "Original File Sizes:"
+du -h /var/log/syslog /var/log/wtmp
+
+mkdir -p "$backup_dir"
+gzip -c /var/log/syslog > "$backup_dir/syslog-$timestamp.gz"
+gzip -c /var/log/wtmp > "$backup_dir/wtmp-$timestamp.gz"
+
+echo "Compressed File Sizes:"
+du -h "$backup_dir/syslog-$timestamp.gz" "$backup_dir/wtmp-$timestamp.gz"
+
+sudo truncate -s 0 /var/log/syslog /var/log/wtmp
+
+echo "Comparison:"
+du -h /var/log/syslog /var/log/wtmp "$backup_dir/syslog-$timestamp.gz" "$backup_dir/wtmp-$timestamp.gz"
+
+
+# End
